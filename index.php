48168bc2e7404f3bb7a935c1602c12cf81a5e3f8
githubsemai <githubsemai@gmail.com>
2023-03-20 17:09:47 +0800

    menambahkan lish

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis rerum magni necessitatibus, error sunt tempora, earum porro consectetur, modi cum vero ea omnis quos. Exercitationem quis porro eligendi distinctio iusto!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
